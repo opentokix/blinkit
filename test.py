2f2f6834ea38c878f2a6b2e3190ff86018522bd0
OpenTokix <petereriksson.net@gmail.com>
2018-01-13 15:45:40 +0100

    initial commit

--- test.py
+++ test.py
@@ -3,15 +3,23 @@
 from blinkt import set_pixel, set_brightness, show, clear
 import time
 
-red = [200, 40, 40]
-green = [ 40, 200, 40]
-blue = [40, 40, 200]
-
+red = [220, 40, 40]
+green = [40, 220, 40]
+blue = [40, 40, 220]
 
 
 def main():
 
-    set_pixel(0, red[0], red[1], red[2])
+    set_pixel(0, blue[0], blue[1], blue[2])
+    set_pixel(1, blue[0], blue[1], blue[2])
+    set_pixel(2, green[0], green[1], green[2])
+    set_pixel(3, green[0], green[1], green[2])
+    set_pixel(4, green[0], green[1], green[2])
+    set_pixel(5, green[0], green[1], green[2])
+    set_pixel(6, red[0], red[1], red[2])
+    set_pixel(7, red[0], red[1], red[2])
+
+
     show()
     time.sleep(10)
 
