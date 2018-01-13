bf6b4d77a8767692f501753de4c2984becf7725d
OpenTokix <petereriksson.net@gmail.com>
2018-01-13 15:42:51 +0100

    initial commit

--- test.py
+++ test.py
@@ -3,17 +3,16 @@
 from blinkt import set_pixel, set_brightness, show, clear
 import time
 
-step = [ 1, 16, 31, 46, 61, 76, 91, 106, 121, 136, 151, 166, 181, 196, 211, 226, 241, 255 ]
+red = [200, 40, 40]
+green = [ 40, 200, 40]
+blue = [40, 40, 200]
+
+
 
 def main():
 
-    for r in step:
-        for g in step:
-            for b in step:
-                for led in range(8):
-                    set_pixel(led, r, g, b)
-                    show()
-#        clear()
+    set_pixel(0, red)
+    show()
 
 
 
