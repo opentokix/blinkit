56ef30190fcdfacd4ef0159e35b9c9bb3a54c831
OpenTokix <petereriksson.net@gmail.com>
2018-01-13 15:16:20 +0100

    initial commit

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 from blinkt import set_pixel, set_brightness, show, clear
+import time
 
 
 def main():
     set_brightness(0.1)
     clear()
-    set_pixel(0, 255, 255, 255)
-    show()
+    for color in range(255):
+        for brightness in range(255):
+            set_pixel(brightness, color, color, color)
+            show()
+
 
 
 if __name__ == '__main__':
