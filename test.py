6942caac40556652ddab79ae1cecdcba7a7dd6e6
OpenTokix <petereriksson.net@gmail.com>
2018-01-13 15:55:19 +0100

    initial commit

--- test.py
+++ test.py
@@ -2,26 +2,67 @@
 
 from blinkt import set_pixel, set_brightness, show, clear
 import time
+import math
 
 red = [220, 40, 40]
 green = [40, 220, 40]
 blue = [40, 40, 220]
 
 
-def main():
-
-    set_pixel(0, blue[0], blue[1], blue[2])
-    set_pixel(1, blue[0], blue[1], blue[2])
-    set_pixel(2, blue[0], blue[1], blue[2])
-    set_pixel(3, green[0], green[1], green[2])
-    set_pixel(4, green[0], green[1], green[2])
-    set_pixel(5, green[0], green[1], green[2])
-    set_pixel(6, red[0], red[1], red[2])
-    set_pixel(7, red[0], red[1], red[2])
-
-
+def update(value):
+    if value < 1:
+        clear()
+    if value == 1:
+        set_pixel(0, blue[0], blue[1], blue[2])
+    if value == 2:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+    if value == 3:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+    if value == 4:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+        set_pixel(3, green[0], green[1], green[2])
+    if value == 5:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+        set_pixel(3, green[0], green[1], green[2])
+        set_pixel(4, green[0], green[1], green[2])
+    if value == 6:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+        set_pixel(3, green[0], green[1], green[2])
+        set_pixel(4, green[0], green[1], green[2])
+        set_pixel(5, green[0], green[1], green[2])
+    if value == 7:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+        set_pixel(3, green[0], green[1], green[2])
+        set_pixel(4, green[0], green[1], green[2])
+        set_pixel(5, green[0], green[1], green[2])
+        set_pixel(6, red[0], red[1], red[2])
+    if value >= 8:
+        set_pixel(0, blue[0], blue[1], blue[2])
+        set_pixel(1, blue[0], blue[1], blue[2])
+        set_pixel(2, blue[0], blue[1], blue[2])
+        set_pixel(3, green[0], green[1], green[2])
+        set_pixel(4, green[0], green[1], green[2])
+        set_pixel(5, green[0], green[1], green[2])
+        set_pixel(6, red[0], red[1], red[2])
+        set_pixel(7, red[0], red[1], red[2])
     show()
-    time.sleep(10)
+
+
+def main():
+    for i in range(100):
+        value = math.sin(i)*4+4
+        print "%d" % int(value)
 
 
 
